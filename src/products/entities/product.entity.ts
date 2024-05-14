7a266a68debb0b4beaead4a856fca22eca2f572a
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-05-13 20:37:14 -0600

    fin seccion 11

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,6 +1,7 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./product-image.entity";
 
-@Entity()
+@Entity({name: 'products'})
 export class Product {
     
     @PrimaryGeneratedColumn('uuid')
@@ -33,6 +34,12 @@ export class Product {
     @Column('text', { array: true, default: [] })
     tags: string[]
 
+    @OneToMany(
+        () => ProductImage,
+        (productImage) => productImage.product,
+        {cascade: true, eager: true}
+    )
+    images?: ProductImage[]
 
 
     @BeforeInsert()
